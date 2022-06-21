b29960fb8297e25139b53c4923a3de3af8ef57e4
zyenapz <zyenapz@gmail.com>
2022-06-21 14:24:28 +0800

    Stash changes

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,11 @@
-enum Difficulty {
-    Easy,
-    Medium,
-    Hard,
-}
-
-impl Difficulty {
-    fn get_max_mistakes(&self) -> u8 {
-        match self {
-            &Difficulty::Easy => game_constants::MAX_MISTAKES_EASY,
-            &Difficulty::Medium => game_constants::MAX_MISTAKES_MEDI,
-            &Difficulty::Hard => game_constants::MAX_MISTAKES_HARD,
-        }
-    }
-}
-
-mod game_constants {
-    pub const QUESTIONS_PER_GAME: u8 = 15;
-    pub const MAX_MISTAKES_EASY: u8 = 6;
-    pub const MAX_MISTAKES_MEDI: u8 = 4;
-    pub const MAX_MISTAKES_HARD: u8 = 3;
-}
+use difficulty::Difficulty;
+use game_data::SessionData;
+use words::Word;
 
 fn jumble_word(word: &String) {
     let word_length: usize = word.len();
 
     // TODO
-}
-
-pub struct SessionData {
-    mistakes: u8,
-    score: u8,
-    difficulty: Difficulty,
-    words: Vec<Word>,
-}
-
-pub struct Word {
-    normal_form: String,
-    jumbled_form: String,
 }
 
 fn main() {
@@ -49,8 +18,49 @@ fn main() {
             jumbled_form: "SDAS".to_string(),
         }],
     };
+}
 
-    data.score += 1;
+mod difficulty {
+    use crate::game_data::game_constants;
 
-    println!("Score: {}", data.score);
+    pub enum Difficulty {
+        Easy,
+        Medium,
+        Hard,
+    }
+
+    impl Difficulty {
+        pub fn get_max_mistakes(&self) -> u8 {
+            match self {
+                &Difficulty::Easy => game_constants::MAX_MISTAKES_EASY,
+                &Difficulty::Medium => game_constants::MAX_MISTAKES_MEDI,
+                &Difficulty::Hard => game_constants::MAX_MISTAKES_HARD,
+            }
+        }
+    }
+}
+
+mod words {
+    pub struct Word {
+        pub(crate) normal_form: String,
+        pub(crate) jumbled_form: String,
+    }
+}
+
+mod game_data {
+    use crate::{difficulty::Difficulty, words::Word};
+
+    pub mod game_constants {
+        pub const QUESTIONS_PER_GAME: u8 = 15;
+        pub const MAX_MISTAKES_EASY: u8 = 6;
+        pub const MAX_MISTAKES_MEDI: u8 = 4;
+        pub const MAX_MISTAKES_HARD: u8 = 3;
+    }
+
+    pub struct SessionData {
+        pub(crate) mistakes: u8,
+        pub(crate) score: u8,
+        pub(crate) difficulty: Difficulty,
+        pub(crate) words: Vec<Word>,
+    }
 }
