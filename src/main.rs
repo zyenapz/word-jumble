276e37108775193d9bff7875f3f5a6ffe24e32a1
zyenapz <zyenapz@gmail.com>
2022-06-22 14:57:38 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,9 @@
-use difficulty::Difficulty;
-use game_data::SessionData;
-use word::Word;
-
-fn jumble_word(word: &String) {
-    let word_length: usize = word.len();
-
-    // TODO
-}
+use std::io;
+use std::io::Write;
 
 fn main() {
-    let mut data: SessionData = SessionData {
-        mistakes: 0,
-        score: 0,
-        difficulty: Difficulty::Easy,
-        words: vec![Word {
-            normal_form: "SDAS".to_string(),
-            jumbled_form: "SDAS".to_string(),
-        }],
-    };
-
     let mut is_running: bool = true;
+    let mut action: String = String::new();
 
     while is_running {
         /* Display welcome screen and options
@@ -29,50 +13,43 @@ fn main() {
         3. Add attributions info at the bottom
         4. Add a way to get the user input
         */
+
+        display_title();
+        display_options();
+
+        io::stdin()
+            .read_line(&mut action)
+            .expect("Error getting input");
+
+        is_running = false;
     }
 }
 
-mod difficulty {
-    use crate::game_data::game_constants;
-
-    pub enum Difficulty {
-        Easy,
-        Medium,
-        Hard,
-    }
-
-    impl Difficulty {
-        pub fn get_max_mistakes(&self) -> u8 {
-            match self {
-                &Difficulty::Easy => game_constants::MAX_MISTAKES_EASY,
-                &Difficulty::Medium => game_constants::MAX_MISTAKES_MEDI,
-                &Difficulty::Hard => game_constants::MAX_MISTAKES_HARD,
-            }
-        }
-    }
+struct Word {
+    pub(crate) normal_form: String,
+    pub(crate) jumbled_form: String,
 }
 
-mod word {
-    pub struct Word {
-        pub(crate) normal_form: String,
-        pub(crate) jumbled_form: String,
-    }
+fn display_title() {
+    println!("Jumble Rust!");
+    println!("------------------------");
+    println!("Version 1.0 | by zyenapz\n");
 }
 
-mod game_data {
-    use crate::{difficulty::Difficulty, word::Word};
+fn display_options() {
+    println!("Select option no.:");
+    println!("[1] Play");
+    println!("[1] Hi-score");
+    println!("[3] Exit");
+    print!("> ");
+    io::stdout().flush().unwrap();
+}
 
-    pub mod game_constants {
-        pub const QUESTIONS_PER_GAME: u8 = 15;
-        pub const MAX_MISTAKES_EASY: u8 = 6;
-        pub const MAX_MISTAKES_MEDI: u8 = 4;
-        pub const MAX_MISTAKES_HARD: u8 = 3;
-    }
+fn jumble_word(word: &String) -> String {
+    let word_length: usize = word.len();
 
-    pub struct SessionData {
-        pub(crate) mistakes: u8,
-        pub(crate) score: u8,
-        pub(crate) difficulty: Difficulty,
-        pub(crate) words: Vec<Word>,
-    }
+    let mut jumbled: String = String::from("");
+
+    return jumbled;
+    // TODO
 }
