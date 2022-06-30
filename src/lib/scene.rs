cfd0cf7c15a476ded076f7f9a01a976a57d90c94
zyenapz <zyenapz@gmail.com>
2022-07-01 00:27:53 +0800

    Update scene.rs
    Added a simple menu screen

--- src/lib/scene.rs
+++ src/lib/scene.rs
@@ -1,6 +1,17 @@
-use std::io;
+use std::{
+    io::{self, Write},
+    ops::Add,
+};
+
+use crate::lib::menu_command::MenuCommand;
 
 use super::game_data::GameData;
+
+macro_rules! clear_console {
+    () => {
+        print!("\x1B[2J\x1B[1;1H");
+    };
+}
 
 pub trait Scene {
     fn handle(self: Box<Self>, data: &mut GameData) -> Box<dyn Scene>;
@@ -14,23 +25,41 @@ pub struct Exit;
 impl Scene for Menu {
     fn handle(self: Box<Self>, data: &mut GameData) -> Box<dyn Scene> {
         let mut is_active = true;
-        let mut next_state: Box<dyn Scene>;
+        let mut next_state: Box<dyn Scene> = self;
+        let mut error_message = String::new();
 
         while is_active {
+            clear_console!();
             println!("Welcome to Word Jumble!!");
+            println!("[1] Play");
+            println!("[2] Credits");
+            println!("[3] Exit");
+            println!("{}", error_message);
+
+            print!("> ");
+            io::stdout()
+                .flush()
+                .expect("Error encountered when flushing output!");
 
             let mut buf = String::new();
             io::stdin()
                 .read_line(&mut buf)
                 .expect("Error encountered when getting input!");
 
-            match buf.trim().to_lowercase().as_str() {
-                "1" => is_active = false,
-                _ => println!("Lol"),
+            // TODO change later
+            match MenuCommand::new(&buf) {
+                Ok(cmd) => {
+                    match cmd {
+                        MenuCommand::Play => next_state = Box::new(Play),
+                        MenuCommand::Credits => next_state = Box::new(Credits),
+                        MenuCommand::Exit => next_state = Box::new(Exit),
+                    }
+                    is_active = false
+                }
+                Err(e) => error_message = String::from(format!(": {}", e)),
             }
         }
 
-        next_state = Box::new(Exit);
         next_state
     }
 }
@@ -38,6 +67,8 @@ impl Scene for Menu {
 impl Scene for Play {
     fn handle(self: Box<Self>, data: &mut GameData) -> Box<dyn Scene> {
         println!("Play");
+        data.set_running_to_inactive();
+
         self
     }
 }
@@ -45,6 +76,8 @@ impl Scene for Play {
 impl Scene for Credits {
     fn handle(self: Box<Self>, data: &mut GameData) -> Box<dyn Scene> {
         println!("Credits");
+        data.set_running_to_inactive();
+
         self
     }
 }
